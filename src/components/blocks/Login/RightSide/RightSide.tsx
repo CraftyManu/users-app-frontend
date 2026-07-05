bb5dbf0e2a32c858bda5cd739211a7b7d43d010f
Manu Sartor <manuelasartor2015@gmail.com>
2026-07-05 11:33:34 -0300

    add RightSide component with styles for layout and image display

---
+++ src/components/blocks/Login/RightSide/RightSide.tsx
@@ -0,0 +1,11 @@
+import styles from './RightSide.module.css'
+
+function LoginRightSide() {
+    return (
+        <section>
+            <img className={styles.rightImage} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRja4WBL5IbFcY3eEHrBixvbnZv6WJGOEDMTCRm0cDAXg&s=10' alt="" />
+        </section>
+    )
+}
+
+export default LoginRightSide
