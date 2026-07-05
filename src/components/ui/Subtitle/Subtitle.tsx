b7c82f0df880bf512c967be17cc7ef2f2be8f507
Manu Sartor <manuelasartor2015@gmail.com>
2026-07-05 10:57:53 -0300

    add Subtitle and Title components with corresponding styles

---
+++ src/components/ui/Subtitle/Subtitle.tsx
@@ -0,0 +1,17 @@
+import styles from './Subtitle.module.css'
+
+interface SubtitleProps {
+    children: React.ReactNode
+}
+
+function Subtitle({
+    children,
+}: SubtitleProps) {
+    return (
+        <p className={styles.subtitle}        >
+            {children}
+        </p>
+    )
+}
+
+export default Subtitle
---
+++ src/components/ui/Title/Title.tsx
@@ -0,0 +1,17 @@
+import styles from './Title.module.css'
+
+interface TitleProps {
+    children: React.ReactNode
+}
+
+function Title({
+    children,
+}: TitleProps) {
+    return (
+        <h1 className={styles.title}        >
+            {children}
+        </h1>
+    )
+}
+
+export default Title
