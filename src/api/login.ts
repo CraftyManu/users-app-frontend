c09f51fac0a06573a00886bd576b56d7b2a29c45
Manu Sartor <manuelasartor2015@gmail.com>
2026-07-25 15:55:22 -0300

    add user management API: implement createUser, getUsers, login, updateUser functions and User type

---
+++ src/api/login.ts
@@ -0,0 +1,25 @@
+import { API_URL } from "@/config/globals"
+
+// -------------------------------------------------
+// POST /auth/login → devuelve { token, role }
+// -------------------------------------------------
+
+export async function login(email: string, password: string) {
+    // 1. Hacemos la petición POST con email y password en ele body
+    const response = await fetch(`${API_URL}/auth/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+    })
+
+    // 2. Convertimos la respuesta a JSON
+    const body = await response.json()
+
+    // 3. Si el backend respondió con error, lanzamos su mensaje
+    if (!body.success) {
+        throw new Error(body.message) // Ej: "Passsword incorrecto"
+    }
+
+    // 4. Devolvemos solo la data: { token, role }
+    return body.data
+}
